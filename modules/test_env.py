7e55e5b309d73ac3e948f473fcd416f330496cdf
Felix Argyle <maksiimys1980@gmail.com>
2024-10-30 13:17:55 +0200

    .

--- modules/test_env.py
+++ modules/test_env.py
@@ -13,7 +13,4 @@ def start_module():
 
     @bot.on(events.NewMessage(from_users=bot.allowed_users, pattern="!ttest"))
     async def test(event):
-        spot = spotdl.Spotdl
-        input_str = event.message.message.replace(f"!ttest", "")
-        result = spot.search(spot, query=[input_str])
-        await bot.send_file(event.chat_id, file=result)
+        pass
