59cc64c4287cde84c1b2a3c04e91fe6925242eb5
Felix Argyle <maksiimys1980@gmail.com>
2024-11-25 19:46:43 +0200

    Вимкнув крону на старт рейду

--- service/scheduler.py
+++ service/scheduler.py
@@ -13,9 +13,9 @@ async def aiocron_scheduler():
     async def process_cleanup():
         await cleanup_temp_files()
 
-    @aiocron.crontab("0 1 * * *")  # Щодня о 01:00
-    async def process_rusak_daily():
-        await feed_work_and_mine()
+    # @aiocron.crontab("0 1 * * *")  # Щодня о 01:00
+    # async def process_rusak_daily():
+    #     await feed_work_and_mine()
 
     @aiocron.crontab("0 8-23 * * *")  # Щогодини з 8 до 23
     async def process_raid_start():
