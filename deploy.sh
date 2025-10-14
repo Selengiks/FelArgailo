8d94889201659bb5f8f558dcf436d320a2766612
Felix Argyle <maksiimys1980@gmail.com>
2025-10-14 05:52:09 +0300

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 git stash
 git fetch
 git reset --hard $1
-docker compose stop fel_argailo
-docker compose rm -f fel_argailo
+docker compose stop FelArgailo
+docker compose rm -f FelArgailo
 docker compose build --no-cache
 docker image prune -f
 docker buildx prune -f
-docker compose up -d fel_argailo
+docker compose up -d FelArgailo
